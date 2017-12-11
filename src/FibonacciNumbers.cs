3099d54ecfe30108cd6b29bc86c4be0f46c12e5c
Horia Coman <horia141@gmail.com>
2017-12-11 14:48:57 +0200

    Add the DPSum algorithm to FibonacciNumbers

--- src/FibonacciNumbers.cs
+++ src/FibonacciNumbers.cs
@@ -7,7 +7,8 @@ namespace CoreAlgos
     /// </summary>
     /// <remarks>
     ///   <para>
-    ///     The Nth Fibonacci <c>F_N</c> is defined as <c>F_{N-1} + F_{N-2}</c> with base cases of <c>F_0 = 0</c> and <c>F_1 = 1</c>.
+    ///     The Nth Fibonacci <c>F_N</c> is defined as <c>F_{N-1} + F_{N-2}</c> with base cases
+    ///     of <c>F_0 = 0</c> and <c>F_1 = 1</c>.
     ///   </para>
     /// </remarks>
     public static class FibonacciNumbers
@@ -17,7 +18,10 @@ namespace CoreAlgos
         /// </summary>
         /// <remarks>
         ///   <para>
-        ///     The time complexity is <c>O(2^n)</c> and the space complexity is <c>O(n)</c>.
+        ///     The time complexity is <c>O(2^n)</c> and the space complexity is <c>O(n)</c>, as this
+        ///     computes the nth number via a direct translation of the formula into code.
+        ///     Technically this is a doubly exponential algorithm, as the storage space to
+        ///     represent <c>n</c> is just <c>q=log(n)</c> bits, making it <c>O(2^2^q)</c>.
         ///   </para>
         /// </remarks>
         /// <param name="n">Which Fibonacci number to generate</param>
@@ -33,6 +37,41 @@ namespace CoreAlgos
                 return 1;
 
             return Naive(n - 1) + Naive(n - 2);
+        }
+
+        /// <summary>
+        ///   Compute the nth Fibonacci number in a fast way, based on Dynamic Programming.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     The time complexity is <c>O(n)</c> and the space complexity is <c>O(1)</c>. Technically,
+        ///     this is an exponential algorithm, as the storage space to represent <c>n</c> is just
+        ///     <c>q=log(n)</c> bits, making it <c>O(2^q)</c>. This is a classic simple DP algorithm,
+        ///     where we build up the nth number incrementally. We can also discard everything past
+        ///     the last two values, so the space is constant.
+        ///   </para>
+        /// </remarks>
+        public static long DPSum(long n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Expected a positive number");
+
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            long Fn = 1;
+            long Fnm1 = 0;
+
+            for (long i = 1; i < n; i++)
+            {
+                long Fnp1 = Fn + Fnm1;
+                Fnm1 = Fn;
+                Fn = Fnp1;
+            }
+
+            return Fn;
         }
     }
 }
--- test/FibonacciNumbersTest.cs
+++ test/FibonacciNumbersTest.cs
@@ -50,5 +50,48 @@ namespace CoreAlgos.Test
         }
 
         #endregion
+
+        #region DPSum
+
+        [Fact]
+        public void DPSumThrowsForNegativeValues()
+        {
+            Action action1 = () => FibonacciNumbers.DPSum(-1);
+            action1.ShouldThrow<ArgumentException>();
+            Action action2 = () => FibonacciNumbers.DPSum(-10);
+            action2.ShouldThrow<ArgumentException>();
+
+        }
+
+        [Fact]
+        public void DPSumBaseCases()
+        {
+            FibonacciNumbers.DPSum(0).Should().Be(0);
+            FibonacciNumbers.DPSum(1).Should().Be(1);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(8, 21)]
+        [InlineData(9, 34)]
+        [InlineData(10, 55)]
+        [InlineData(11, 89)]
+        [InlineData(12, 144)]
+        [InlineData(13, 233)]
+        [InlineData(14, 377)]
+        [InlineData(15, 610)]
+        public void DPSumWorks(long n, long fibn)
+        {
+            FibonacciNumbers.DPSum(n).Should().Be(fibn);
+        }
+
+        #endregion
     }
 }
